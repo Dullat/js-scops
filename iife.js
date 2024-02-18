b0c8144efeda87fda8acea128b7155fe313d07a2
dullat <dullat@users.noreply.github.com>
2024-02-18 17:05:38 +0530

    update

--- iife.js
+++ iife.js
@@ -1,10 +1,45 @@
 // immediately invoke function expression
 // it prevent global polution, executed immediately also allow us to wrap functions, provide clean code
 
-(function print(){
+(function print() {
   console.log("h");
 })();
 
 (() => {
   console.log("arow")
 })()
+
+//example1
+
+let calculator = (function () {
+  return function (a, b) {
+    let add = () => a + b;
+    let sub = () => a - b;
+    let mul = () => a * b;
+    let div = () => a / b;
+
+    return { add, sub, mul, div }
+  }
+})();
+
+console.log(calculator(12, 3).add());
+
+//or
+
+let calc1 = calculator(14, 12);
+console.log(calc1.sub());
+
+//example2
+
+let calculator2 = (function () {
+  let add = (a, b) => a + b;
+  let sub = (a, b) => a - b;
+  let mul = (a, b) => a * b;
+  let div = (a, b) => a / b;
+
+  return {add, sub, mul, div}
+})();
+
+let result = calculator2.add(12,15);
+
+console.log(result);
