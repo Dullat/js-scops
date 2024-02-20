d9870aec8e65208aa3344eb8f5087f286b942056
dullat <jashandullat8@users.noreply.github.com>
2024-02-20 22:19:52 +0530

    update

--- get-set.js
+++ get-set.js
@@ -20,3 +20,24 @@ user.fullName = "alice dullat";
 console.log(user.fullName);
 
 console.log(user.name);
+
+
+// define property
+
+let player = {
+    name: "idk",
+    lvl: "10",
+}
+
+Object.defineProperty(player, "status", {
+    get(){
+        return `Name = ${this.name} and lvl = ${this.lvl}`;
+    }
+});
+
+console.log(Object.getPrototypeOf(player));
+
+console.log(player.status);
+
+
+//studied at https://javascript.info/property-accessors
