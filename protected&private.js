357e072980eb4345a653a91c668aae3a0836cd87
DuLLatJatt <142017979+Dullat@users.noreply.github.com>
2024-02-23 16:07:39 +0000

    update

---
+++ protected&private.js
@@ -0,0 +1,18 @@
+class user{
+    #id;
+    name = "";
+    constructor(name){
+        this.name = name;
+        this.#id = Math.floor(Math.random() * 1200);
+    }
+
+    getDetails() {
+        console.log({name: this.name, id: this.#id});
+    }
+}
+
+let user1 = new user("dullat");
+user1.getDetails();
+
+//node protected&private.js <-- does not gonna work in node coz of &
+//so use: node 'protected&private.js'
