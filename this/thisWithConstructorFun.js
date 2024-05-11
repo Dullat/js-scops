1caddee9dfb2c0ee3c4a20b134d5fa7f8c9d9f40
DuLLatJatt <142017979+Dullat@users.noreply.github.com>
2024-05-11 23:59:32 +0530

    Update thisWithConstructorFun.js

--- this/thisWithConstructorFun.js
+++ this/thisWithConstructorFun.js
@@ -18,7 +18,7 @@ function Person2(n) {
   this.name = n
   setTimeout(() => {
     console.log(this)
-  }, 2000)
+  }, 2000) // Arrow functions do not create their own execution context for the this keyword. Instead, they inherit the this value from the enclosing lexical context where they are defined. This behavior is sometimes referred to as lexical scoping or lexical this binding.
 }
 
 const p2 = new Person2("hahah")
